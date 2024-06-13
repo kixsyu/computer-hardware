3c66e0fa03a48258e70aade1f04145ab2e762acd
pista666 <158519687+pista666@users.noreply.github.com>
2024-06-13 12:34:15 -0700

    13/6

--- script.js
+++ script.js
@@ -14,4 +14,15 @@ function toggleBinaryInfo() {
     } else {
         info.style.display = 'none';
     }
+
+    const image = document.getElementById('myImage');
+
+    // Example: Change image source on button click
+    const changeImageButton = document.createElement('button');
+    changeImageButton.innerText = 'Change Image';
+    changeImageButton.onclick = function() {
+        image.src = 'https://www.totalphase.com/media/blog/2022/08/Intel-CPU1.jpg';
+    };
+
+    document.body.appendChild(changeImageButton);
 }
